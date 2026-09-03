cd9ccc6286bf8dae87a918314680b4f83ab9e6e4
Ubuntu <ubuntu@ip-172-31-4-237.eu-north-1.compute.internal>
2026-09-03 06:03:19 +0000

    fix server.js

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
-{
-  "name": "jenkins-node-demo",
-  "version": "1.0.0",
-  "description": "Node.js Jenkins Docker demo",
-  "main": "server.js",
-  "scripts": {
-    "start": "node server.js",
-    "test": "node -e \"console.log('Tests passed')\""
-  },
-  "dependencies": {}
-}
+const http = require('http');
+
+const PORT = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {
+        'Content-Type': 'text/plain'
+    });
+
+    res.end('Hello, World!\n');
+});
+
+server.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server running on port ${PORT}`);
+});
